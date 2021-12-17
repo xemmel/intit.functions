59ab0e0735a685ea77d5e6e91281800bf88b4b40
Morten la Cour <lacour@gmail.com>
2021-12-17 08:03:10 +0100

    Mytrigger changes

--- intitFunctions/Functions/MyTrigger.cs
+++ intitFunctions/Functions/MyTrigger.cs
@@ -4,34 +4,34 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace intitFunctions
+namespace intitFunctions;
+
+public class MyTrigger
 {
-    public class MyTrigger
+    private readonly ILogger _logger;
+    private readonly IFunctionHandler _functionHandler;
+
+    public MyTrigger(
+            ILoggerFactory loggerFactory,
+            IFunctionHandler functionHandler)
     {
-        private readonly ILogger _logger;
-        private readonly IFunctionHandler _functionHandler;
+        _logger = loggerFactory.CreateLogger<MyTrigger>();
+        _functionHandler = functionHandler;
+    }
 
-        public MyTrigger(
-                ILoggerFactory loggerFactory, 
-                IFunctionHandler functionHandler)
-        {
-            _logger = loggerFactory.CreateLogger<MyTrigger>();
-            _functionHandler = functionHandler;
-        }
+    [Function("MyTrigger")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        var xslt = _functionHandler
+                        .GetHeader(request: req, key: "xslt");
+        _logger.LogInformation($"xslt: {xslt}");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        [Function("MyTrigger")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            var xslt = _functionHandler
-                            .GetHeader(request: req,key: "xslt");
-            _logger.LogInformation($"xslt: {xslt}");
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString($"Welcome to Azure Functions!\tVersion: {_functionHandler.GetVersion()}");
 
-            response.WriteString($"Welcome to Azure Functions!\tVersion: {_functionHandler.GetVersion()}");
-
-            return response;
-        }
+        return response;
     }
 }
+
